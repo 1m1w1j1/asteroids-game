6378610ded8fa34dc77f480a7d49bc4f9c9db030
Mark Jones <1mark1w1jones1@gmail.com>
2026-02-15 19:35:25 -0500

    removed white spaces

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from constants import SCREEN_HEIGHT
 
 def main():
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
-    print("Screen width: ", SCREEN_WIDTH)
-    print("Screen height: ", SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
