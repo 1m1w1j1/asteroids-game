0a8d00ed486dc361194baee3ce6c4f3d74e99fa3
Mark Jones <1mark1w1jones1@gmail.com>
2026-02-20 11:35:20 -0500

    refactoring

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
-from constants import SCREEN_WIDTH
-from constants import SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-
-
+from player import Player
 
 def main():
     print("Starting Asteroids with pygame version: ", pygame.version.ver)
@@ -13,20 +11,22 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        fps_clock.tick(60) 
+        
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        fps_clock.tick(60)
         dt = fps_clock.get_time() / 1000.0
         #print("dt: ", dt)
-
-    screen.fill("black")
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
-    player.draw(screen)
-    pygame.display.flip()
-
 
 if __name__ == "__main__":
     main()
